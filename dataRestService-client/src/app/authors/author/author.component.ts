26a430cba7b72487d68cffe74c0af76e70b613e0
TyroneShawn <31389512+TyroneShawn@users.noreply.github.com>
2024-11-03 21:04:32 -0500

    Added Authors

---
+++ dataRestService-client/src/app/authors/author/author.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from "rxjs";
+import { Author } from '../../books/model/book';
+import { ActivatedRoute } from '@angular/router';
+import { AuthorsService } from '../service/authors.service';
+
+
+@Component({
+  selector: 'app-author',
+  templateUrl: './author.component.html',
+  styleUrls: ['./author.component.css']
+})
+export class AuthorComponent implements OnInit, OnDestroy {
+
+  selectedAuthor!: Author | null;
+  private subscription!: Subscription;
+
+  constructor(private route: ActivatedRoute, private authorService: AuthorsService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.subscription = this.authorService.getAuthor(id).subscribe({
+        next: (data: Author) => {
+          this.selectedAuthor = data;
+        },
+        error: (_: any) => {
+          this.selectedAuthor = null;
+        }
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+}
